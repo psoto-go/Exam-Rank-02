ba8eac208523891e83c6ebc76aa79f7c9f679999
Pablo Soto <pabletesglez@gmail.com>
2021-11-25 14:16:37 +0100

    add.

--- inter/inter_prueba.c
+++ inter/inter_prueba.c
@@ -1,77 +1,66 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   union.c                                            :+:      :+:    :+:   */
+/*   inter_prueba.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: psoto-go <psoto-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/22 15:11:47 by psoto-go          #+#    #+#             */
-/*   Updated: 2021/11/25 12:04:19 by psoto-go         ###   ########.fr       */
+/*   Created: 2021/11/25 10:55:27 by psoto-go          #+#    #+#             */
+/*   Updated: 2021/11/25 11:10:51 by psoto-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void    ft_putchar(char c)
+void     ft_putchar(char c)
 {
     write(1, &c, 1);
 }
 
-int     checker(char *str, char c, int len)
+int     check(char *str, char c, int index)
 {
     int i;
 
     i = 0;
-    while (i < len)
+    while (i < index)
     {
-        if (str[i] == c)
-            return (1);
+        if(str[i] == c)
+            return(1);
         i++;
     }
     return (0);
 }
 
-void    ft_union(char *str1, char *str2)
+void    inter(char *str, char *str2)
 {
     int i;
-	int n;
-	int flag;
+    int n;
 
     i = 0;
-	n = 0;
-	while (str1[i])
+    while (str[i])
     {
-        if (checker(str1, str1[i], i) == 0)
-            ft_putchar(str1[i]);
+        if(check(str, str[i], i) == 0)
+        {
+            n = 0;
+            while (str2[n])
+            {
+                if(str[i] == str2[n])
+                {
+                    ft_putchar(str[i]);
+                    break;
+                }
+                n++;
+            }
+        }
         i++;
-    }
-    while (str2[n])
-    {
-        if(checker(str2, str2[n], n) == 0)
-		{
-			i = 0;
-			flag = 0;
-			while (str1[i])
-			{
-				if (str2[n] == str1[i])
-				{
-					flag = 1;
-					break;
-				}
-				i++;
-			}
-			if (flag == 0)
-				ft_putchar(str2[n]);
-		}
-		n++;
     }
     
 }
 
 int     main(int argc, char **argv)
 {
-    if (argc != 3)
+    if(argc != 3)
         ft_putchar('\n');
     else
-        ft_union(argv[1], argv[2]);
+        inter(argv[1], argv[2]);
 }
