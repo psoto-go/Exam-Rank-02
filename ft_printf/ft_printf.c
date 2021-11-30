a21217784412f08f57a8b2c17648e6e8d629ff8d
psoto-go <pabletesglez@gmail.com>
2021-11-30 19:01:21 +0100

    git add

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -5,157 +5,105 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: psoto-go <psoto-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/25 12:17:33 by psoto-go          #+#    #+#             */
-/*   Updated: 2021/11/25 14:50:23 by psoto-go         ###   ########.fr       */
+/*   Created: 2021/11/30 15:56:11 by psoto-go          #+#    #+#             */
+/*   Updated: 2021/11/30 18:57:52 by psoto-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <unistd.h>
 #include <stdarg.h>
-#include <stdio.h>
+#include <unistd.h>
+#include "ft_printf.h"
 
-void    ft_putchar(char c, int *res)
+void	ft_putchar(int c, int *res)
 {
-    *res = *res + 1;
-    write(1, &c, 1);
+	*res = *res + 1;
+	write(1, &c, 1);
 }
 
-void    ft_putnbr(int num, int *res)
+void	ft_write(char *str, int *res)
 {
-    if(num > -2147483648 && num <= 2147483647)
-    {
-        if (num == -2147483648)
-        {
-            ft_putchar('-', res);
-            ft_putchar('2', res);
-            ft_putnbr(147483648, res);
-        }
-        else if (num >= 10)
-        {
-            ft_putnbr(num / 10, res);
-            ft_putnbr(num % 10, res);
+	char 	*aux;
+	int		i;
 
-        }
-        else if (num < 0)
-        {
-            num = -num;
-            ft_putnbr(num / 10, res);
-            ft_putnbr(num % 10, res);
-        }
-        else
-            ft_putchar(num + '0', res);
-    }
-}
-
-void ft_write(char *str, int *res)
-{
-    char *aux;
-    int i;
-
-    i = 0;
-    if (str == NULL){
-        aux = "(null)";
-    }else
-        aux = str;
-    while (aux[i])
-    {
-        write(1, &aux[i], 1);
+	i = 0;
+	if (str == NULL)
+		aux = "(null)";
+	else
+		aux = str;
+	while (aux[i])
+	{
+		write(1, &aux[i], 1);
 		i++;
-		*res += 1;
-    }
+		*res = *res + 1;
+	}
 }
 
-char    *ft_detohe(unsigned long int num)
+void	ft_putnbr_base(long int num, int *res, char *base, long long int n)
 {
-    int     len;
-    int     co;
-    int     aux;
-    char	*string;
-    int     i;
-
-    aux = num;
-    len = 1;
-    while (aux >= 16)
-    {
-        co = aux / 16;
-        aux = co;
-        len++;
-    }
-    string = malloc(sizeof(char) * (len + 1));
-    if (!string)
-        return(0);
-    if (num == 0)
-        string[len - 1] = '0';
-    i = len - 1;
-    co = 0;
-    while (num != 0){
-        co = num % 16;
-        if (co < 10)
-			co += 48;
-		else if (co >= 10)
-			co += 87;
-        string[i--] = co;
-		num /= 16;
-    }
-    string[len] = '\0';
-	return (string);
-    
+	if (num < 0)
+	{
+		ft_putchar('-', res);
+		num *= -1;
+	}
+	if (num >= n)
+		ft_putnbr_base(num / n, res, base, n);
+	ft_putchar(base[num % n], res);
 }
 
-int checker(char c, char c1, va_list args)
+void	ft_putnbr_basex(long long int num, int *res, char *base, long long int n)
 {
-    int res;
-
-    res = 0;
-    if (c == '%' && c1 == 's')
-        ft_write(va_arg(args, char *), &res);
-    else if (c == '%' && c1 == 'd')
-        ft_putnbr(va_arg(args, int), &res);
-    else if (c == '%' && c1 == 'x')
-        ft_write(ft_detohe(va_arg(args, unsigned int)), &res);
-    else if (c == '%' && c1 =='%')
-        ft_putchar('%', &res);
-    return (res);
+	if (num >= n)
+		ft_putnbr_basex(num / n, res, base, n);
+	ft_putchar(base[num % n], res);
 }
 
-int ft_printf(const char *var, ...)
+int		checker(char c, char c1, va_list args)
 {
-    va_list args;
-    int i;
-    int res;
+	int res;
 
-    va_start(args, var);
-    i = 0;
-    res = 0;
-    while (var[i])
-    {
-        if (var[i] == '%')
-        {
-            res += checker(var[i], var[i + 1], args);
-            i++;
-        }
-        else
-        {
-            ft_putchar(var[i], &res);
-        }
-        i++;
-    }
-    va_end(args);
-    return (res);
+	res = 0;
+	if (c == '%' && c1 == 's')
+		ft_write(va_arg(args, char *), &res);
+	if (c == '%' && c1 == 'd')
+		ft_putnbr_base(va_arg(args, int), &res, "0123456789", 10);
+	if (c == '%' && c1 == 'x')
+		ft_putnbr_basex(va_arg(args, unsigned int), &res, "0123456789abcdef", 16);
+	return(res);
 }
-int main()
+
+int		ft_printf(const char *var, ... )
 {
-    int i = 0;
-    int j = 0;
-    i =  printf("Hexadecimal for %d is %x\n", 42, 42);
-    j =  ft_printf("Hexadecimal for %d is %x\n", 42, 42);
-    printf("%d suyo %d\n", i, j);
-    ft_printf("%d suyo %d\n", i, j);
+	va_list args;
+	int 	res;
+	int 	i;
 
-    ft_printf("%s\n", "toto");
-
-    ft_printf("Magic %s is %d", "number", 42);
-
-    ft_printf("Hexadecimal for %d is %x\n", 42, 42);
+	va_start(args, var);
+	res = 0;
+	i = 0;
+	while (var[i])
+	{
+		if(var[i] == '%')
+		{
+			res += checker(var[i], var[i + 1], args);
+			i++;
+		}
+		else
+			ft_putchar(var[i], &res);
+		i++;
+	}
+	va_end(args);
+	return (res);
 }
+
+// int 	main()
+// {
+// 	int a;
+
+// 	a = ft_printf("%s %d %x\n", "hola", 1221, -123123);
+// 	printf("%s %d %x\n", "hola", 1221, -123123);
+// 	printf("%s %d %x\n", "hola", 1221, 123123);
+
+// 	// ft_printf("%s %d", "hola", 1221);
+// 	ft_printf("%d\n", a);
+// 	printf("%d\n", a);
+// }
---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psoto-go <psoto-go@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/24 10:43:33 by psoto-go          #+#    #+#             */
+/*   Updated: 2021/11/30 18:16:32 by psoto-go         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <limits.h>
+
+int		ft_printf(const char *var, ...);
+
+
+
+#endif
