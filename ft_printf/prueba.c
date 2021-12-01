c286568bf9c809e5e7941463044a9da966881055
psoto-go <pabletesglez@gmail.com>
2021-12-01 15:55:55 +0100

    add

--- ft_printf/prueba.c
+++ ft_printf/prueba.c
@@ -1,17 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   prueba.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: psoto-go <psoto-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/30 15:56:11 by psoto-go          #+#    #+#             */
-/*   Updated: 2021/12/01 10:35:12 by psoto-go         ###   ########.fr       */
+/*   Created: 2021/12/01 12:17:41 by psoto-go          #+#    #+#             */
+/*   Updated: 2021/12/01 12:37:09 by psoto-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
-#include <unistd.h>
 #include "ft_printf.h"
 
 void	ft_putchar(int c, int *res)
@@ -25,64 +23,66 @@ void	ft_write(char *str, int *res)
 	char 	*aux;
 	int		i;
 
-	i = 0;
-	if (str == NULL)
+	if (!str)
 		aux = "(null)";
 	else
 		aux = str;
-	while (aux[i])
+	while (str[i])
 	{
-		write(1, &aux[i], 1);
+		ft_putchar(str[i],res);
 		i++;
-		*res = *res + 1;
 	}
 }
 
-void	ft_putnbr_base(long long int num, int *res, char *base, long long int n)
+void	ft_putnbr(int long long num, int *res, char *base, int n)
 {
 	if (num < 0)
 	{
-		ft_putchar('-', res);
 		num *= -1;
+		ft_putchar('-', res);
 	}
 	if (num >= n)
-		ft_putnbr_base(num / n, res, base, n);
+		ft_putnbr(num / n, res, base, n);
 	ft_putchar(base[num % n], res);
+
 }
 
-void	ft_putnbr_basex(long long int num, int *res, char *base, long long int n)
+void	ft_putnbrx(int long long num, int *res, char *base, int n)
 {
 	if (num >= n)
-		ft_putnbr_basex(num / n, res, base, n);
+		ft_putnbr(num / n, res, base, n);
 	ft_putchar(base[num % n], res);
+
 }
+
+
 
 int		checker(char c, char c1, va_list args)
 {
-	int res;
+	int		res;
 
 	res = 0;
 	if (c == '%' && c1 == 's')
 		ft_write(va_arg(args, char *), &res);
 	if (c == '%' && c1 == 'd')
-		ft_putnbr_base(va_arg(args, int), &res, "0123456789", 10);
+		ft_putnbr(va_arg(args, int), &res, "0123456789", 10);
 	if (c == '%' && c1 == 'x')
-		ft_putnbr_basex(va_arg(args, unsigned int), &res, "0123456789abcdef", 16);
-	return(res);
+		ft_putnbr(va_arg(args, unsigned int), &res, "0123456789abcdef", 16);
+	return (res);
 }
 
-int		ft_printf(const char *var, ... )
+int 	ft_printf(const char *var, ... )
 {
 	va_list args;
-	int 	res;
+	int		res;
 	int 	i;
 
 	va_start(args, var);
-	res = 0;
 	i = 0;
+	res = 0;
 	while (var[i])
 	{
-		if(var[i] == '%')
+		if (var[i] == '%')
 		{
 			res += checker(var[i], var[i + 1], args);
 			i++;
@@ -91,19 +91,16 @@ int		ft_printf(const char *var, ... )
 			ft_putchar(var[i], &res);
 		i++;
 	}
+
 	va_end(args);
-	return (res);
+	return(res);
 }
 
-// int 	main()
-// {
-// 	int a;
-
-// 	a = ft_printf("%s %d %x\n", "hola", 1221, -123123);
-// 	printf("%s %d %x\n", "hola", 1221, -123123);
-// 	printf("%s %d %x\n", "hola", 1221, 123123);
-
-// 	// ft_printf("%s %d", "hola", 1221);
-// 	ft_printf("%d\n", a);
-// 	printf("%d\n", a);
-// }
+int 	main()
+{
+	int a = 0;
+	int b = 0;
+	a = ft_printf("%s %d %x %d %x\n", "hola soy pablo", 223423423123, 223423423123, -223423423123, -223423423123);
+	b = printf("%s %d %x %d %x\n", "hola soy pablo", 223423423123, 223423423123, -223423423123, -223423423123);
+	printf("%d  %d", a, b);
+}
